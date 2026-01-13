0d646ec42eec84abbf98111f2916b7ef2c630d76
Krittaya-Th <krittayaclf9028@gmail.com>
2026-01-13 19:27:11 +0700

    lab14_2_completed

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -20,4 +20,43 @@ int main(){
 	return 0;
 }
 
-//Do not modify source code above this line 
+void showMatrix(const bool B[][N]){
+    for(int a=0;a<N;a++){
+        for(int b=0;b<N;b++){
+            cout<<B[a][b]<<" ";
+        }
+        cout<<endl;
+    }
+    
+}
+
+void inputMatrix(double M[][N]){
+    for(int i=0;i<N;i++){
+        cout<<"Row "<<i+1<<": ";    
+        for(int j=0;j<N;j++){
+            cin>>M[i][j];
+        }
+    }
+    
+}
+
+void findLocalMax(const double A[][N], bool B[][N]){
+    for(int i=0;i<N;i++){
+        for(int j=0;j<N;j++){
+            B[i][j]=false;
+		}	
+
+	}
+	for(int a=1;a<N-1;a++){
+        for(int b=1;b<N-1;b++){
+            if(A[a][b]>=A[a-1][b]&&
+               A[a][b]>=A[a+1][b]&&
+               A[a][b]>=A[a][b-1]&&
+               A[a][b]>=A[a][b+1]){
+				B[a][b]=true;
+			}
+		}	
+
+	}
+	
+}
